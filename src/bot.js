0f28afc6723c31e102b8f7adafc83e2a5fdd038a
sonichigo <pathakanimesh12@gmail.com>
2021-07-13 17:25:53 +0530

    UNBAN FEATURE

--- src/bot.js
+++ src/bot.js
@@ -34,7 +34,18 @@ client.on('message',(message) =>{
         } else {
          return message.channel.send('That member was not found');
       }
-    } 
+    } else if (CMD_NAME === 'ban') {
+        if (!message.member.hasPermission('BAN_MEMBERS'))
+          return message.reply("You do not have permissions to use that command");
+        if (args.length === 0) return message.reply("Please provide an ID");
+        try {
+          const user = message.guild.members.ban(args[0]);
+          message.channel.send('User was banned successfully');
+        } catch (err) {
+          console.log(err);
+          message.channel.send('An error occured. Either I do not have permissions or the user was not found');
+        }
+      } 
     }
 });
     /*  else if(CMD_NAME === "unban"){
