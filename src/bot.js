bc47632ff24db5ab664a43a118a27db996aa0c9f
sonichigo <pathakanimesh12@gmail.com>
2021-07-13 17:23:58 +0530

    KICK FEATURE

--- src/bot.js
+++ src/bot.js
@@ -34,27 +34,6 @@ client.on('message',(message) =>{
         } else {
          return message.channel.send('That member was not found');
       }
-    } else if (CMD_NAME === 'ban') {
-        if (!message.member.hasPermission('BAN_MEMBERS'))
-          return message.reply("You do not have permissions to use that command");
-        if (args.length === 0) return message.reply("Please provide an ID");
-        try {
-          const user = message.guild.members.ban(args[0]);
-          message.channel.send('User was banned successfully');
-        } catch (err) {
-          console.log(err);
-          message.channel.send('An error occured. Either I do not have permissions or the user was not found');
-        }
-      }
-    /*  else if(CMD_NAME === "unban"){
-        let member = client.user.fetch(unbanned.ag);
-
-        if (member == null) {
-          message.reply('Cannot find a ban for the given user.');
-          return;
-        }
-        message.guild.members.unban(member, 'Unbanned by ' + message.author.tag).then(user => {
-          message.channel.send('Unbanned <@' + ag + '>.');
-        }).catch(() => console.error);}*/
+    } 
     }
 });
